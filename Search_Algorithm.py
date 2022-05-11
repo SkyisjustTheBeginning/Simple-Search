f48f99c51a39770f218341f784751cb0ba1fbd5f
Jon Mathews <87596318+SkyisjustTheBeginning@users.noreply.github.com>
2022-05-11 20:00:42 +0530

    Update Search_Algorithm.py

--- Search_Algorithm.py
+++ Search_Algorithm.py
@@ -1,5 +1,5 @@
-arr = [1,2,3,4,5,6,7,70985,70689695,87,6,4,5,6,35,67,45,67,6,67,4,6,6,54,67,""]
-y = input("Enter Element to be found:")
+arr = [1,2,3,4,5,6,7,70985,70689695,87,6,4,5,6,35,67,45,67,6,67,4,6,6,54,67]
+y = int(input("Enter Element to be found:"))
 if y in arr:
     print("Element Found")
     print("Index of Element:" , arr.index(y))
